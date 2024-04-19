775e124468f88ca72b5277bd402ab9832f296187
Daniel Kaminski <daniel.kamin.23@gmail.com>
2024-04-18 20:53:40 -0400

    Did test files, everything works

--- test/test.py
+++ test/test.py
@@ -30,3 +30,68 @@ async def test_project(dut):
   #await ClockCycles(dut.clk, 1)
 
   #assert dut.uo_out.value == 50
+
+  #Set the input values, wait one clock cycle, and check the output
+  dut._log.info("ADD Test")
+  dut.ui_in.value = 98
+  dut.uio_in.value = 0
+  
+  await ClockCycles(dut.clk, 10)
+
+  assert dut.uo_out.value == 134
+
+  
+  dut._log.info("SUB Test")
+  dut.ui_in.value = 98
+  dut.uio_in.value = 1
+
+  await ClockCycles(dut.clk, 10)
+  assert dut.uo_out.value == 72
+
+
+  dut._log.info("SRA Test")
+  dut.ui_in.value = 208
+  dut.uio_in.value = 2
+
+  await ClockCycles(dut.clk, 10)
+  assert dut.uo_out.value == 234
+
+
+  dut._log.info("SRL Test")
+  dut.ui_in.value = 208
+  dut.uio_in.value = 3
+
+  await ClockCycles(dut.clk, 10)
+  assert dut.uo_out.value == 104
+
+
+  dut._log.info("SLL Test")
+  dut.ui_in.value = 208
+  dut.uio_in.value = 4
+
+  await ClockCycles(dut.clk, 10)
+  assert dut.uo_out.value == 170
+
+
+  dut._log.info("AND Test")
+  dut.ui_in.value = 219
+  dut.uio_in.value = 5
+
+  await ClockCycles(dut.clk, 10)
+  assert dut.uo_out.value == 146
+
+
+  dut._log.info("AND Test Zero")
+  dut.ui_in.value = 208
+  dut.uio_in.value = 5
+
+  await ClockCycles(dut.clk, 10)
+  assert dut.uo_out.value == 1
+
+
+  dut._log.info("OR Test")
+  dut.ui_in.value = 221 
+  dut.uio_in.value = 6
+
+  await ClockCycles(dut.clk, 10)
+  assert dut.uo_out.value == 210
